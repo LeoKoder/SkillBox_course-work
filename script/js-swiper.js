aa0cadcdb50be7cb8ee32bff23031d46e0c2d0a3
LeoKot <git@github.com:LeoKoder>
2021-01-17 09:17:39 +0300

    added section gallery anв catalog

--- script/js-swiper.js
+++ script/js-swiper.js
@@ -1,4 +1,21 @@
-var swiper = new Swiper(".swiper-container", {
+var swiper = new Swiper(".hero__slider", {
   direction: "horizontal",
   loop: true,
+});
+
+var swiper = new Swiper(".gallery__slider", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  wrapperEL: {
+    el: ".gallery__wrapper",
+  },
+  pagination: {
+    el: ".gallery-pagination",
+    type: "fraction",
+  },
+  autoHeight: false,
 });
