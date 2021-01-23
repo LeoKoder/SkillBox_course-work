51a88cf1469e7fe1c9541f9416544e851ed0ca5d
LeoKot <git@github.com:LeoKoder>
2021-01-24 01:09:22 +0300

    update maps and added customScroll in header

--- script/script.js
+++ script/script.js
@@ -15,10 +15,41 @@ window.addEventListener("DOMContentLoaded", function () {
       // Порядок по умолчанию: «широта, долгота».
       // Чтобы не определять координаты центра карты вручную,
       // воспользуйтесь инструментом Определение координат.
-      center: [55.76, 37.64],
+      center: [55.75736198732715, 37.60850385455312],
+      controls: [],
       // Уровень масштабирования. Допустимые значения:
       // от 0 (весь мир) до 19.
-      zoom: 7,
+      zoom: 16,
     });
+    // Создание метки произвольного цвета.
+    var myPlacemark = new ymaps.Placemark(
+      [55.75823317538601, 37.601068771307105],
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "img/marker.png",
+        iconImageSize: [20, 20],
+      }
+    );
+
+    myMap.controls.add("geolocationControl", {
+      size: "small",
+      float: "right",
+      position: {
+        bottom: "310px",
+        right: "20px",
+      },
+    });
+
+    myMap.controls.add("zoomControl", {
+      size: "small",
+      float: "right",
+      position: {
+        top: "275px",
+        right: "20px",
+      },
+    });
+    // Размещение геообъекта на карте.
+    myMap.geoObjects.add(myPlacemark);
   }
 });
